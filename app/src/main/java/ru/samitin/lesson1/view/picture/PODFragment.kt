664bb284678fb2791ca2003cf9ddfb834930c1bb
samitin <samitin.e@gmail.com>
2021-09-08 23:11:42 +1100

    Разобрались с Fab+ButtonAppBar

--- app/src/main/java/ru/samitin/lesson1/view/picture/PODFragment.kt
+++ app/src/main/java/ru/samitin/lesson1/view/picture/PODFragment.kt
@@ -3,18 +3,19 @@ package ru.samitin.lesson1.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.samitin.lesson1.R
 import ru.samitin.lesson1.databinding.FragmentMainBinding
+import ru.samitin.lesson1.view.MainActivity
 import ru.samitin.lesson1.viewModel.PictureOfTheDayData
 import ru.samitin.lesson1.viewModel.PODViewModel
 
@@ -36,6 +37,7 @@ class PODFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getLiveData().observe(viewLifecycleOwner, Observer { renderDATA(it) })
         viewModel.sendServerRequest()
+        setBottomAppBar(view)
         binding.inputLayout.setEndIconOnClickListener {
             val intent=Intent(Intent.ACTION_VIEW).apply {
                 data= Uri.parse("https://ru.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
@@ -44,7 +46,7 @@ class PODFragment : Fragment() {
         }
         bottomSheetBehavior=BottomSheetBehavior.from(binding.includeLayout.bottomSheetContainer)
         bottomSheetBehavior.state=BottomSheetBehavior.STATE_HALF_EXPANDED
-        bottomSheetBehavior.addBottomSheetCallback(object :
+        /*bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when (newState) {
@@ -60,7 +62,7 @@ class PODFragment : Fragment() {
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
                 TODO("not implemented")
             }
-        })
+        })*/
     }
     private fun renderDATA(data:PictureOfTheDayData){
         when (data){
@@ -83,4 +85,56 @@ class PODFragment : Fragment() {
             return PODFragment()
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_search -> Toast.makeText(context, "Search", Toast.LENGTH_SHORT).show()
+            // у нашего бургера такой вот id внутри android
+            android.R.id.home-> BottomNavigationDraverFragment().show(requireActivity().supportFragmentManager,"TAG")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private var isMain=true
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+        binding.fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                binding.bottomAppBar.navigationIcon = null // лучше придумать замену бургеру
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_back_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                binding.bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_hamburger_menu_bottom_bar
+                    )
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+    }
+
 }
