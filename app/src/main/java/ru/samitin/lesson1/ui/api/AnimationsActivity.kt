db8a0bb43bbb75f04348ad0b7db453d7df86d4ae
samitin <59145062+samitin@users.noreply.github.com>
2021-09-28 15:09:16 +1100

    Урок 5: Перемешивание объектов

--- app/src/main/java/ru/samitin/lesson1/ui/api/AnimationsActivity.kt
+++ app/src/main/java/ru/samitin/lesson1/ui/api/AnimationsActivity.kt
@@ -5,39 +5,52 @@ import android.os.Bundle
 import android.view.Gravity
 
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
+
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.transition.*
 
-import ru.samitin.lesson1.databinding.ActivityAnimationsEnlargeBinding
-import ru.samitin.lesson1.databinding.ActivityAnimationsPathTransitionsBinding
+import ru.samitin.lesson1.databinding.ActivityAnimationsShuffleBinding
 
 
 class AnimationsActivity : AppCompatActivity() {
 
     private var toRightAnimation = false
 
-    var _binding:ActivityAnimationsPathTransitionsBinding ?=null
+    var _binding:ActivityAnimationsShuffleBinding ?=null
     val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityAnimationsPathTransitionsBinding.inflate(layoutInflater)
+        _binding= ActivityAnimationsShuffleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val titles:MutableList<String> = ArrayList()
+        for (i in 0..4){
+            titles.add(String.format("item %d",i+1))
+        }
+        createViews(binding.transitionsContainer,titles)
 
         binding.button.setOnClickListener {
             val changeBounds=ChangeBounds()
-            changeBounds.setPathMotion(ArcMotion())
-            changeBounds.duration=500
+            changeBounds.duration=1000//Время анимации
             TransitionManager.beginDelayedTransition(binding.transitionsContainer,changeBounds)
-            toRightAnimation= !toRightAnimation
-            val params=binding.button.layoutParams as FrameLayout.LayoutParams
-            params.gravity= if(toRightAnimation)Gravity.RIGHT or Gravity.BOTTOM else Gravity.START or Gravity.TOP
-            binding.button.layoutParams=params
+            titles.shuffle()//перемешать
+            createViews(binding.transitionsContainer,titles)
         }
     }
 
+    private fun createViews(layout: ViewGroup, titles: List<String>) {
+        layout.removeAllViews()//Очищает контейнер
+        for (title in titles){
+            val textView=TextView(this)
+            textView.text=title
+            textView.gravity=Gravity.CENTER_HORIZONTAL
+            ViewCompat.setTransitionName(textView,title)
+            layout.addView(textView)
+        }
+    }
 
 
 }
