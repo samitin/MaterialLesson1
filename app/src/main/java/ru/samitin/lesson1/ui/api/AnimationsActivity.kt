385eb9ca0f9c93dd02f0fcfdbc5285a840814f3a
samitin <59145062+samitin@users.noreply.github.com>
2021-09-28 14:33:34 +1100

    Урок 5: Движение по траектории

--- app/src/main/java/ru/samitin/lesson1/ui/api/AnimationsActivity.kt
+++ app/src/main/java/ru/samitin/lesson1/ui/api/AnimationsActivity.kt
@@ -1,45 +1,40 @@
 package ru.samitin.lesson1.ui.api
 
-import android.graphics.Rect
+
 import android.os.Bundle
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
+
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.*
 
-import ru.samitin.lesson1.R
-
-import ru.samitin.lesson1.databinding.ActivityAnimationsBinding
 import ru.samitin.lesson1.databinding.ActivityAnimationsEnlargeBinding
-import ru.samitin.lesson1.databinding.ActivityAnimationsExplodeBinding
-import androidx.recyclerview.widget.RecyclerView.ViewHolder as ViewHolder
+import ru.samitin.lesson1.databinding.ActivityAnimationsPathTransitionsBinding
+
 
 class AnimationsActivity : AppCompatActivity() {
 
-    private var isExpanded = false
+    private var toRightAnimation = false
 
-    var _binding:ActivityAnimationsEnlargeBinding ?=null
+    var _binding:ActivityAnimationsPathTransitionsBinding ?=null
     val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding= ActivityAnimationsEnlargeBinding.inflate(layoutInflater)
+        _binding= ActivityAnimationsPathTransitionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.imageView.setOnClickListener {
-            isExpanded= !isExpanded
-            TransitionManager.beginDelayedTransition(
-                binding.container,
-                TransitionSet().addTransition(ChangeBounds()).addTransition(ChangeImageTransform())
-            )
-            val params:ViewGroup.LayoutParams=binding.imageView.layoutParams
-            params.height=if(isExpanded)ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
-            binding.imageView.layoutParams=params
-            binding.imageView.scaleType = if (isExpanded) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.FIT_CENTER
+        binding.button.setOnClickListener {
+            val changeBounds=ChangeBounds()
+            changeBounds.setPathMotion(ArcMotion())
+            changeBounds.duration=500
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer,changeBounds)
+            toRightAnimation= !toRightAnimation
+            val params=binding.button.layoutParams as FrameLayout.LayoutParams
+            params.gravity= if(toRightAnimation)Gravity.RIGHT or Gravity.BOTTOM else Gravity.START or Gravity.TOP
+            binding.button.layoutParams=params
         }
     }
 
